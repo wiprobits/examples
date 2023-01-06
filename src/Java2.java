349e8020e479a0e7df060a4da6e827cf59033c5d
SU20359564 <SU20359564@APL-5CD8184T16>
2023-01-06 08:27:57 +0530

    added two more java files

---
+++ src/Java2.java
@@ -0,0 +1,10 @@
+
+public class Java2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome Wipro");
+
+	}
+
+}
---
+++ src/Java3.java
@@ -0,0 +1,9 @@
+
+public class Java3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome Spring Boot");
+	}
+
+}
